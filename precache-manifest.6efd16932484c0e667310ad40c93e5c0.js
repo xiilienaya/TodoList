4096f93f1b9fb2142167fa57321b675820c6ed56
anwenyue <1624572613@qq.com>
2019-10-19 13:05:29 +0800

    anwenyue

---
+++ precache-manifest.6efd16932484c0e667310ad40c93e5c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4580e63d0f9912cfffe898cc6101a193",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0730dec5b1ab7b96d5d5",
+    "url": "/static/css/main.df018ecb.chunk.css"
+  },
+  {
+    "revision": "82e6b0e21a4f5db05d15",
+    "url": "/static/js/2.8e96d2aa.chunk.js"
+  },
+  {
+    "revision": "0730dec5b1ab7b96d5d5",
+    "url": "/static/js/main.c875a843.chunk.js"
+  },
+  {
+    "revision": "867559b179ea8c7af62a",
+    "url": "/static/js/runtime-main.6a03bc6f.js"
+  }
+]);
